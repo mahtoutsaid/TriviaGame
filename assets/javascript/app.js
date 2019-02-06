69e8b0cb9da5907b33705a549c706652895f8c45
mahtoutsaid <mahtoutsaid0913@gmail.com>
2019-02-05 18:50:30 -0800

    added 02-05-19

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,8 @@
 
-$('.btn').on('click', function(){
-	$('#myDiv').show();
-	$(this).hide();
-});
+// $('.btn').on('click', function(){
+// 	$('#myDiv').show();
+// 	$(this).hide();
+// });
 
 
  
@@ -17,43 +17,93 @@ $('.btn').on('click', function(){
 		  url: "https://opentdb.com/api.php?amount=10&type=multiple",
 		  method: "GET"
 		}).then(function(response) {
-			// $('#question').html(response.results[0].question)
-		 //    $('#answer1').html(response.results[0].incorrect_answers[0])
-		 //    $('#answer2').html(response.results[0].incorrect_answers[1])
-		 //    $('#answer3').html(response.results[0].incorrect_answers[2])
-			// $('#answer4').html(response.results[0].correct_answer)
-
-			// console.log(response.results[0].question)
-		 //    console.log(response.results[0].correct_answer)
+		
 		    console.log(response.results.length)
 
-     $('#btn').on('click', function(){
-     	var random_question = Math.floor(Math.random()*(response.results.length))
+     $('#btn').on('click', function nextQuestion(){
+         $('#myDiv').show();
+	     $(this).hide();
+
+     	var random_question = Math.floor(Math.random()*(response.results.length));
      	var question = response.results[random_question].question;
-     	   
+
+     	var incorrectAnswers = response.results[random_question].incorrect_answers;
+     	var correctAnswer = response.results[random_question].correct_answer;
+     	var arr_answers = incorrectAnswers.concat(correctAnswer);
+     	console.log(arr_answers);
+     	var i = arr_answers.length;
+     	var random_index, num;
+
+       $('#question').text(question);
+
+     	while(i > 0) {
+     	 random_index = Math.floor(Math.random() * i);
+     	i--
+
+        num = arr_answers[i];
+        arr_answers[i] = arr_answers[random_index];
+        arr_answers[random_index] = num;
+       
+           
+     	}
+     	
+			console.log(arr_answers)
 			
-			$('#question').text(question);
+
+		arr_answers.forEach(function(h){
 			
-          
-			// var par = $('.answer').html(response.results[random_question].incorrect_answers);
-			// $('myDiv').append(par)
-			$('myDiv').append(response.results[random_question].correct_answer)
-			console.log('correct_answer  '+response.results[random_question].correct_answer)
-            
-        
-			console.log('incorrect_answers  '+response.results[random_question].incorrect_answers)
-			console.log('correct_answer  '+response.results[random_question].correct_answer)
+			var par = $('<p>');
+			par.text(h);
+			$('#myDiv').append(par);
+           
 
 		})
 
+		  $('p').on('click', function(){
+            	if($(this).text() == correctAnswer){
+				alert('you won')
+				$('p').text("")
+                nextQuestion()
+			}else{
+				alert('you lost')
+				$('p').text("")
+                nextQuestion()
+			}
+			
+            })	     
 
+			console.log('correct_answer  '+response.results[random_question].correct_answer)
+            console.log('incorrect_answers  '+response.results[random_question].incorrect_answers)
+			
 
-
+		})
 
 		});
 
+
+
+
 		
-	
+//  function getTime(){
+
+
+// var s = 0;
+// var m = 2;
+
+
+// setTimeout('getTime()', 1000)
+
+// if (m<10){
+// 	m= '0' + m
+// }
+// if (s<10){
+// 	s= '0' + s
+// }
+
+// $('#time_remaining').html(h+':'+m+':'+s)
+
+// }
+// getTime()	 
 
 
 	
